2d5541ece7e8b1bd995a4154ae5c25f93d56698f
Jon Lefcheck <jslefche@vims.edu>
2014-08-04 10:29:09 -0400

    Improved handling of model formulae
    Formulae and model data now returned correctly for all model classes

--- R/get_lavaan_sem.R
+++ R/get_lavaan_sem.R
@@ -1,12 +1,14 @@
 get.lavaan.sem = function(modelList, add.vars = NULL) {
 
-  model = do.call(paste,c(lapply(modelList, function(i) 
-    if(class(i) == "lme") 
-      paste(formula(i)[[2]], formula(i)[[1]], formula(i)[[3]]) else
-        paste(formula(i)[[2]], "~", paste(names(fixef(i))[-1], collapse = "+")) ), 
-    sep = "\n"))
-  
+  model = paste(unlist(lapply(modelList, function(i) paste(format(formula(i)), collapse = ""))), collapse = "\n")
+
   if(!is.null(add.vars)) model = paste(model, 
     paste(unname(sapply(add.vars, function(x) as.formula(paste(x, x, sep="~")))), collapse="\n"), sep = "\n")
   
-  sem(model,data=if(class(modelList[[1]]) == "lme") modelList[[1]]$data else modelList[[1]]@frame) }
+  if(all(class(i) %in% c("lm", "glm", "negbin"))) model.data = i$model else 
+    if(all(class(i) %in% c("lme", "glmmPQL"))) model.data = i$data else
+      if(all(class(i) %in% c("lmerMod", "merModLmerTest", "glmerMod"))) model.data = i@frame
+  
+  sem(model, model.data) 
+
+}
