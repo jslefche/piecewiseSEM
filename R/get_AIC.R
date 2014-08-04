1c5c0f68356f35406ca3f52d987d127cf169f763
Jon Lefcheck <jslefche@vims.edu>
2014-08-04 11:56:42 -0400

    Revised function name

--- R/get_AIC.R
+++ R/get_AIC.R
@@ -15,11 +15,8 @@ get.aic = function(modelList, pvalues.df = NULL, adjust.p = FALSE, .progressBar 
   if(length(basis.set) < 1) 
     warning("All endogenous variables are conditionally dependent: no test of d-sep necessary")
   
-  if(is.null(pvalues.df)) {
-    
-    if(.progressBar == T & length(basis.set) > 1) pb = txtProgressBar(min = 0, max = length(basis.set), style = 3) else pb = NULL
-    
-    pvalues.df = get.missing.paths(modelList, adjust.p, .progressBar, basis.set, add.vars) }
+  if(is.null(pvalues.df))
+    pvalues.df = get.missing.paths(modelList, adjust.p, .progressBar, basis.set, add.vars)
   
   fisher.c = get.fisher.c(modelList, pvalues.df, adjust.p, .progressBar, basis.set, add.vars)
   
