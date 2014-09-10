f56134a37be63b3ac033846d334aa0ee694a8d20
Jon Lefcheck <jslefche@vims.edu>
2014-09-10 10:39:27 -0400

    Added corr errors to lavaan

--- R/get_lavaan_sem.R
+++ R/get_lavaan_sem.R
@@ -1,16 +1,17 @@
-get.lavaan.sem = function(modelList, add.vars = NULL) {
+get.lavaan.sem = function(modelList, data, corr.errors = NULL, add.vars = NULL) {
 
-  sem.model = paste(unlist(lapply(modelList, function(model) paste(format(formula(model)), collapse = ""))), collapse = "\n")
+  sem.model = paste(c(unlist(lapply(modelList, function(i) {
+    
+    if(all(class(i) %in% c("lmerMod", "merModLmerTest", "glmerMod"))) 
+      form = format(nobars(formula(i))) else
+        form = format(formula(i))
+    
+    paste(form, collapse = "") } ) ), corr.errors), collapse = "\n")
 
-  if(!is.null(add.vars)) sem.model = paste(sem.model, 
-    paste(unname(sapply(add.vars, function(x) as.formula(paste(x, x, sep="~")))), collapse="\n"), sep = "\n")
+  if(!is.null(add.vars)) 
+    
+    sem.model = paste(sem.model, paste(unname(sapply(add.vars, function(x) as.formula(paste(x, x, sep="~")))), collapse="\n"), sep = "\n")
   
-  model = modelList[[which.max(lapply(modelList, nobs))]]
-  
-  if(all(class(model) %in% c("lm", "glm", "negbin"))) sem.model.data = model$sem.model else 
-    if(all(class(model) %in% c("lme", "glmmPQL"))) sem.model.data = model$data else
-      if(all(class(model) %in% c("lmerMod", "merModLmerTest", "glmerMod"))) sem.model.data = model@frame
-  
-  sem(sem.model, sem.model.data) 
+  sem(sem.model, data)
 
 }
