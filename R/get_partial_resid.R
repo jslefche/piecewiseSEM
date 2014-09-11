9e46b3ae8e26e29d30281d0c24d10ff13b9f84a1
Jon Lefcheck <jslefche@vims.edu>
2014-09-11 10:18:48 -0400

    Fixed handling of scaled vars
    Replace now works for variables that are transformed in the equation
    with parantheses

--- R/get_partial_resid.R
+++ R/get_partial_resid.R
@@ -5,7 +5,7 @@ get.partial.resid = function(y, x, modelList) {
   
   y.model = modelList[[match(y, unlist(lapply(modelList, function(i) as.character(formula(i)[2]))))]]
   
-  if(is.null(y.model)) stop("Check spelling of correlated variables")
+  if(is.null(y.model)) stop("Check spelling of correlated variables - must match exactly response in model formula!")
   
   if(all(x %in% as.character(formula(y.model)[[3]]))) stop("Y is a direct function of X, no partial residuals obtainable")
   
@@ -14,8 +14,11 @@ get.partial.resid = function(y, x, modelList) {
   if(all(class(y.model) %in% c("lme", "glmmPQL"))) 
     y.nox.model = update(y.model, fixed = formula(y.nox.formula)) else
       y.nox.model = update(y.model, formula = formula(y.nox.formula))
-                   
-  x.noy.formula = gsub(y, x, y.nox.formula)
+
+  y.replace = gsub("\\(", "\\\\(", y)
+  y.replace = gsub("\\)", "\\\\)", y.replace)
+  
+  x.noy.formula = gsub(y.replace, x, y.nox.formula)
   
   if(all(class(y.model) %in% c("lme", "glmmPQL"))) 
     x.noy.model = update(y.model, fixed = formula(x.noy.formula)) else
