29e83b7712dbb60a074542d3497a6426d6b51fad
Jon Lefcheck <jslefche@vims.edu>
2015-06-16 17:31:40 -0400

    Revert sem.model.fits to old code
    Revert get.model.fits to old code while testing new code (in
    sem.model.fits2)

--- R/sem.model.fits.R
+++ R/sem.model.fits.R
@@ -1,348 +1,146 @@
-sem.model.fits = function(modelList) {
+sem.model.fits <- function(modelList) {
+  if( class(modelList) != "list" ) modelList = list(modelList) else modelList
+  # Iterate over each model in the list
+  do.call(rbind, lapply(modelList, r.squared))
+}
 
-  # If object is just an individual model, convert to a list
-  if(class(modelList) != "list") modelList = list(modelList)
+r.squared <- function(mdl){
+  UseMethod("r.squared")
+}
 
-  # Apply functions across all models in the model list
-  do.call(rbind, lapply(modelList, function(model) {
-    
-    # Create return data.frame
-    ret = data.frame(
-      Class = class(model)[1],
-      Family = "gaussian",
-      Link = "identity",
-      Marginal = NA,
-      Conditional = NA,
-      AIC = AIC(model) 
-    )
-  
-    # Get R2 for class == lm
-    if(any(class(model) == "lm")) ret$Marginal = summary(model)$r.squared
-      
-    # Get R2 for class == glm
-    if(any(class(model) %in% c("glm", "gls", "pgls"))) {
-      
-      # Classify model family
-      if(class(model) == "glm") ret$Family = summary(model)$family[[1]]
-      
-      # Classify link function
-      if(class(model) == "glm")ret$Link = summary(model)$family[[2]]
-      
-      # Calculate McFadden R2 values
-      ret$Marginal = 1 - (as.numeric(logLik(model)) / as.numeric(logLik(update(model, ". ~ 1"))))
-                            
-      # Calculate model AIC
-      ret$AIC = AIC(model)
-      
-    }
-    
-    # Get R2 for class == merMod
-    if(any(class(model) %in% c("lmerMod", "merModLmerTest"))) {
-      
-      # Get variance of fixed effects by multiplying coefficients by design matrix
-      varF = var(as.vector(fixef(model) %*% t(model@pp$X)))
-   
-      # Separate observation variance from variance of random effects
-      n.obs = names(unlist(lapply(ranef(model), nrow))[!unlist(lapply(ranef(model), nrow)) == nrow(Fmat)])
-      
-      # Get variance of random effects 
-      varRand = sum(
-        
-        sapply(VarCorr(model)[n.obs], function(Sigma) { #sapply(VarCorr(model)[n.obs], function(Sigma) {
-          
-          X = model.matrix(model)  
-          
-          Z = X[, rownames(Sigma), drop = FALSE]
-          
-          sum(diag(Z %*% Sigma %*% t(Z)))/nrow(X)
-          
-        } )
-        
-      )
-      
-      # Get residual variance
-      varResid = attr(VarCorr(model), "sc")^2
-      
-      # Calculate R2 values
-      ret$Marginal = varF / (varF + varRand + varResid)
-        
-      ret$Conditional = (varF + varRand) / (varF + varRand + varResid)
-      
-      # Calculate model AIC
-      ret$AIC = AIC(update(model, REML = FALSE))
-      
-    }
-    
-    # Get R2 for class == lme
-    if(all(class(model) == "lme")) {
-      
-      # Get design matrix of fixed effects from model
-      Fmat = model.matrix(eval(model$call$fixed)[-2], model$data)
-      
-      # Get variance of fixed effects by multiplying coefficients by design matrix
-      varF = var(as.vector(fixef(model) %*% t(Fmat)))
- 
-      # Get variance of random effects
-      if(any(class(try(getVarCov(model), silent = TRUE)) == "try-error")) {
-      
-        Sigma = suppressWarnings(as.numeric(VarCorr(model)[, 1]))
-      
-        names(Sigma) = gsub(" =", "", rownames(VarCorr(model)))
-        
-        Sigma = as.list(na.omit(Sigma[-length(Sigma)]))
-          
-        Sigma = lapply(1:length(Sigma), function(i) matrix(Sigma[[i]], dimnames = list(names(Sigma)[i], names(Sigma)[i])) )
-        
-        varRand = sum(
-          
-          sapply(Sigma, function(Sigma1) { 
-            
-            Z = Fmat[, colnames(Sigma1), drop = FALSE]
-            
-            sum(diag(Z %*% Sigma1 %*% t(Z))) / nrow(Fmat)
-            
-          } )
-          
-        )
-        
-      } else {
-        
-        Z = Fmat[, rownames(VarCorr(model))[which(rownames(VarCorr(model)) %in% colnames(Fmat))], drop = FALSE]
-        
-        varRand = sum(diag(Z %*% getVarCov(model) %*% t(Z))) / nrow(Fmat)
-        
-      }
-      
-      # Get residual variance
-      varResid = summary(model)$sigma^2
-      
-      # Calculate R2 values
-      ret$Marginal = varF / (varF + varRand + varResid)
-      
-      ret$Conditional = (varF + varRand) / (varF + varRand + varResid)
-      
-      # Calculate model AIC
-      ret$AIC = AIC(update(model, method = "ML"))
-      
-    }
-    
-    # Get R2 for class == "glmerMod"
-    if(any(class(model) == "glmerMod")) {
-      
-      # Classify model family
-      ret$Family = summary(model)$family
-      
-      # Classify link function
-      ret$Link = summary(model)$link
-      
-      # Get variance of fixed effects by multiplying coefficients by design matrix
-      varF = var(as.vector(fixef(model) %*% t(model@pp$X)))
-      
-      # Separate observation variance from variance of random effects
-      n.obs = names(unlist(lapply(ranef(model), nrow))[!unlist(lapply(ranef(model), nrow)) == nrow(Fmat)])
-      
-      # Get variance of random effects 
-      varRand = sum(
-        
-        sapply(VarCorr(model)[n.obs], function(Sigma) {
-          
-          X = model.matrix(model)  
-          
-          Z = X[, rownames(Sigma), drop = FALSE]
-          
-          sum(diag(Z %*% Sigma %*% t(Z)))/nrow(X)
-          
-        } )
-        
-      )
-      
-      # Get overdispersion variance
-      obs = names(unlist(lapply(ranef(model), nrow))[unlist(lapply(ranef(model), nrow)) == nrow(Fmat)])
-      
-      if(length(obs) == 0) varDisp = 0 else {
-        
-        varDisp =  sum(
-          
-          sapply(VarCorr(model)[obs], function(Sigma) {
-            
-            X = model.matrix(model)  
-            
-            Z = X[, rownames(Sigma)]
-            
-            sum(diag(Z %*% Sigma %*% t(Z)))/nrow(X)
-            
-          } )
-          
-        )
-        
-      }
-      
-      # Get distribution-specific variance
-      if(ret$Family == "binomial") {
-        
-        if(ret$Link == "logit") varDist = (pi^2)/3
-        
-        else if(ret$Link == "probit") varDist = 1 else {
-          
-          warning(paste("Model link '", summary(model)$link, "' is not yet supported for the ", summary(model)$family, "distribution"))
-          
-          varDist = NA
-          
-        }
-        
-      } else if(ret$Family == "poisson") {
-        
-        # Generate null model (intercept and random effects only, no fixed effects)
-        null.model = update(model, formula = paste(". ~ ", get.random.formula(model, "~1", modelList = NULL)))
-        
-        # Get the fixed effects of the null model
-        null.fixef = as.numeric(fixef(null.model))
-        
-        if(ret$Link == "log") varDist = log(1 + 1/exp(null.fixef))
-        
-      } else if(ret$Link == "sqrt") varDist = 0.25 else {
-        
-        warning(paste("Model link '", summary(model)$link, "' is not yet supported for the ", summary(model)$family, "distribution"))
-        
-        varDist = NA
-        
-      }
-      
-      # Calculate R2 values
-      ret$Marginal = varF / (varF + varRand + varDisp + varDist)
-      
-      ret$Conditional = (varF + varRand) / (varF + varRand + varDisp + varDist)
-      
-      # Calculate model AIC
-      ret$AIC = AIC(model)
-      
-    }
-    
-    # Get R2 for class == "glmmPQL"
-    if(any(class(model) == "glmmPQL")) {
-      
-      # Classify model family
-      ret$Family = summary(model)$family[1]
-      
-      # Classify link function
-      ret$Link = summary(model)$family[2]
-      
-      # Get design matrix of fixed effects from model
-      Fmat = model.matrix(eval(model$call$fixed)[-2], model$data)
-      
-      # Get variance of fixed effects by multiplying coefficients by design matrix
-      varF = var(as.vector(fixef(model) %*% t(Fmat)))
-      
-      
-      
-      
-      
-      # Get random effects design matrix
-      rand.mat = ranef(model)
-      
-      # If not in a list, convert and name levels
-      if(!any(class(rand.mat) %in% "list")) {
-        
-        rand.mat = list(rand.mat)
-        
-        names(rand.mat) = attr(unclass(getVarCov(model)), "group.levels")
-        
-      }
-      
-      # Separate observation variance from variance of random effects
-      n.obs = names(rand.mat)[!sapply(rand.mat, nrow) == nrow(Fmat)]
-      
-      Sigma = if(length(n.obs) == 1) as.numeric(VarCorr(model)[1]) else
-        
-        as.numeric(VarCorr(model)[match(paste(n.obs, "="), rownames(VarCorr(model))) + 1, 1])
-      
-      names(Sigma) = if(length(n.obs) == 1) rownames(VarCorr(model))[1] else 
-        
-        rownames(VarCorr(model))[which(rownames(VarCorr(model)) %in% paste(n.obs, "=")) + 1]
+r.squared.lm <- function(mdl){
+  data.frame(Class=class(mdl), Family="gaussian", Link="identity",
+             Marginal=summary(mdl)$r.squared,
+             Conditional=NA, AIC=AIC(mdl))
+}
 
-      # Get variance of random effects, excluding observation variance
-      varRand = sum(
-        
-        sapply(Sigma, function(Sigma1) { 
-          
-          X = model.matrix(model)  
-          
-          Z = Fmat[, names(Sigma1), drop = FALSE]
-          
-          sum(diag(Z %*% Sigma1 %*% t(Z))) / nrow(Fmat)
-          
-        } )
-        
-      )
-      
-      
-      
-      
-      
-  
-      # Get overdispersion variance
-      obs = names(rand.mat)[sapply(rand.mat, nrow) == nrow(Fmat)]
-
-      if(length(obs) == 0) varDisp = 0 else {
-        
-        varDisp =  sum(
-          
-          sapply(VarCorr(model)[obs], function(Sigma) {
-            
-            Z = Fmat[, rownames(Sigma), drop = FALSE]
-            
-            sum(diag(Z %*% Sigma %*% t(Z))) / nrow(Fmat)
-            
-          } )
-          
-        )
-        
-      }
-      
-      # Get distribution-specific variance
-      if(ret$Family == "binomial") {
-        
-        if(ret$Link == "logit") varDist = (pi^2)/3
-        
-        else if(ret$Link == "probit") varDist = 1 else {
-
-          warning(paste("Model link '", summary(model)$family[2], "' is not yet supported for the ", summary(model)$family[1], "distribution"))
-          
-          varDist = NA
-          
-        }
-        
-      } else if(ret$Family == "poisson") {
-        
-        # Generate null model (intercept and random effects only, no fixed effects)
-        null.model = update(model, fixed = ". ~ 1")
-        
-        # Get the fixed effects of the null model
-        null.fixef = as.numeric(fixef(null.model))
-        
-        if(ret$Link == "log") varDist = log(1 + 1/exp(null.fixef))
-        
-      } else if(ret$Link == "sqrt") varDist = 0.25 else {
-        
-        warning(paste("Model link '", summary(model)$family[2], "' is not yet supported for the ", summary(model)$family[1], "distribution"))
-        
-        varDist = NA
-        
-      }
-      
-      # Calculate R2 values
-      ret$Marginal = varF / (varF + varRand + varDisp + varDist)
-      
-      ret$Conditional = (varF + varRand) / (varF + varRand + varDisp + varDist)
-      
-      # Calculate model AIC
-      ret$AIC = NA
-      
+r.squared.merMod <- function(mdl){
+  # Get variance of fixed effects by multiplying coefficients by design matrix
+  VarF <- var(as.vector(lme4::fixef(mdl) %*% t(mdl@pp$X)))
+  # Get variance of random effects by extracting variance components
+  # Omit random effects at the observation level, variance is factored in later
+  VarRand <- sum(
+    sapply(
+      VarCorr(mdl)[!sapply(unique(unlist(strsplit(names(ranef(mdl)),":|/"))), function(l) length(unique(mdl@frame[,l])) == nrow(mdl@frame))],
+      function(Sigma) {
+        X <- model.matrix(mdl)
+        Z <- X[,rownames(Sigma)]
+        sum(diag(Z %*% Sigma %*% t(Z)))/nrow(X) } ) )
+  # Get the dispersion variance
+  VarDisp <- unlist(VarCorr(mdl)[sapply(unique(unlist(strsplit(names(ranef(mdl)),":|/"))), function(l) length(unique(mdl@frame[,l])) == nrow(mdl@frame))])
+  if(is.null(VarDisp)) VarDisp = 0 else VarDisp = VarDisp
+  if(inherits(mdl, "lmerMod")){
+    # Get residual variance
+    VarResid <- attr(lme4::VarCorr(mdl), "sc")^2
+    # Get ML model AIC
+    mdl.aic <- AIC(update(mdl, REML=F))
+    # Model family for lmer is gaussian
+    family <- "gaussian"
+    # Model link for lmer is identity
+    link <- "identity"
+  }
+  else if(inherits(mdl, "glmerMod")){
+    # Get the model summary
+    mdl.summ <- summary(mdl)
+    # Get the model's family, link and AIC
+    family <- mdl.summ$family
+    link <- mdl.summ$link
+    mdl.aic <- AIC(mdl)
+    # Pseudo-r-squared for poisson also requires the fixed effects of the null model
+    if(family=="poisson") {
+      # Get random effects names to generate null model
+      rand.formula <- reformulate(sapply(findbars(formula(mdl)),
+                                         function(x) paste0("(", deparse(x), ")")),
+                                  response=".")
+      # Generate null model (intercept and random effects only, no fixed effects)
+      null.mdl <- update(mdl, rand.formula)
+      # Get the fixed effects of the null model
+      null.fixef <- as.numeric(lme4::fixef(null.mdl))
     }
-    
-    # Return results
-    return(ret)
-    
-  } ) )
-      
+  }
+  # Call the internal function to do the pseudo r-squared calculations
+  .rsquared.glmm(VarF, VarRand, VarResid, VarDisp, family = family, link = link,
+                 mdl.aic = mdl.aic,
+                 mdl.class = class(mdl),
+                 null.fixef = null.fixef)
+}
+
+r.squared.lme <- function(mdl){
+  # Get design matrix of fixed effects from model
+  Fmat <- model.matrix(eval(mdl$call$fixed)[-2], mdl$data)
+  # Get variance of fixed effects by multiplying coefficients by design matrix
+  VarF <- var(as.vector(nlme::fixef(mdl) %*% t(Fmat)))
+  # First, extract variance-covariance matrix of random effects
+  Sigma.list = VarCorr(mdl)[!grepl(" =",rownames(VarCorr(mdl))) & rownames(VarCorr(mdl)) != "Residual", colnames(VarCorr(mdl))=="Variance", drop=F]
+  corr.list = as.numeric(VarCorr(mdl)[!grepl(" =",rownames(VarCorr(mdl))) & rownames(VarCorr(mdl)) != "Residual" & rownames(VarCorr(mdl)) != "(Intercept)",colnames(VarCorr(mdl))=="Corr",drop=F])
+  Sigma.list2 = split(as.numeric(Sigma.list), cumsum(rownames(Sigma.list) == "(Intercept)"), drop=F)
+  Sigma.list2 = lapply(1:length(Sigma.list2), function(i) { 
+    mat = matrix(prod(Sigma.list2[[i]])*abs(corr.list[i]), ncol=length(Sigma.list2[[i]]), nrow=length(Sigma.list2[[i]]))
+    diag(mat) = Sigma.list2[[i]]
+    colnames(mat) = rownames(Sigma.list)[1:sum(cumsum(rownames(Sigma.list) == "(Intercept)") == 1)]
+    rownames(mat) = colnames(mat)
+    return(mat) } )
+  # Calculate variance of random effects
+  VarRand = sum(
+    sapply(
+      Sigma.list2,
+      function(Sigma) {
+        Z <- Fmat[,colnames(Sigma),drop=F]
+        sum(diag(Z %*% Sigma %*% t(Z)))/nrow(Fmat) } ) )
+  # Get residual variance
+  VarResid <- as.numeric(nlme::VarCorr(mdl)[rownames(nlme::VarCorr(mdl))=="Residual", 1])
+  # Call the internal function to do the pseudo r-squared calculations
+  .rsquared.glmm(VarF, VarRand, VarResid, VarDisp, family = "gaussian", link = "identity",
+                 mdl.aic = AIC(update(mdl, method="ML")),
+                 mdl.class = class(mdl))
+}
+
+.rsquared.glmm <- function(varF, varRand, varResid = NULL, varDisp = NULL, family, link,
+                           mdl.aic, mdl.class, null.fixef = NULL){
+  if(family == "gaussian"){
+    # Only works with identity link
+    if(link != "identity")
+      family_link.stop(family, link)
+    # Calculate marginal R-squared (fixed effects/total variance)
+    Rm <- varF/(varF+varRand+varResid)
+    # Calculate conditional R-squared (fixed effects+random effects/total variance)
+    Rc <- (varF+varRand)/(varF+varRand+varResid)
+  }
+  else if(family == "binomial"){
+    # Get the distribution-specific variance
+    if(link == "logit")
+      varDist <- (pi^2)/3
+    else if(link == "probit")
+      varDist <- 1
+    else
+      family_link.stop(family, link)
+    # Calculate marginal R-squared
+    Rm <- varF/(varF+varRand+varDist+varDisp)
+    # Calculate conditional R-squared (fixed effects+random effects/total variance)
+    Rc <- (varF+varRand)/(varF+varRand+varDist+varDisp)
+  }
+  else if(family == "poisson"){
+    # Get the distribution-specific variance
+    if(link == "log")
+      varDist <- log(1+1/exp(null.fixef))
+    else if(link == "sqrt")
+      varDist <- 0.25
+    else
+      family_link.stop(family, link)
+    # Calculate marginal R-squared
+    Rm <- varF/(varF+varRand+varDist+varDisp)
+    # Calculate conditional R-squared (fixed effects+random effects/total variance)
+    Rc <- (varF+varRand)/(varF+varRand+varDist+varDisp)
+  }
+  else
+    family_link.stop(family, link)
+  # Bind R^2s into a matrix and return with AIC values
+  data.frame(Class=mdl.class, Family = family, Link = link,
+             Marginal=Rm, Conditional=Rc, AIC=mdl.aic)
+}
+
+#' stop execution if unable to calculate variance for a given family and link
+family_link.stop <- function(family, link){
+  stop(paste("Don't know how to calculate variance for",
+             family, "family and", link, "link."))
 }
