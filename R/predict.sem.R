9ed06384fff23196e2ad92da6623f5db7b65568d
Jon Lefcheck <jslefche@vims.edu>
2015-06-19 11:24:10 -0400

    Added se.fit to predict.sem

--- R/predict.sem.R
+++ R/predict.sem.R
@@ -1,4 +1,4 @@
-predict.sem = function(modelList, newdata, ...) {
+predict.sem = function(modelList, newdata, se.fit = FALSE, ...) {
   
   # Send newdata to each model in the model list and return output as a data.frame 
   predict.df = do.call(cbind, lapply(modelList, function(i) {
@@ -6,7 +6,7 @@ predict.sem = function(modelList, newdata, ...) {
     # Get model predictions
     if(any(class(i) %in% c("lm", "glm", "neg.bin", "gls", "pgls")) )
        
-       predict.df = predict(i, newdata, ...) else
+       predict.df = predict(i, newdata, se.fit = se.fit, ...) else
          
          if(any(class(i) %in% c("lme", "glmmPQL"))) 
            
@@ -16,20 +16,47 @@ predict.sem = function(modelList, newdata, ...) {
                
                predict.df = predict(i, newdata, re.form = NA, ...)
     
-    # If predictions are stored in a list, bind columns
-    if(class(predict.df) == "list") predict.df = do.call(data.frame, predict.df[1:2]) else
-      
-      predict.df = data.frame(predict.df)
-  
-    # Name columns
-    if(ncol(predict.df) == 1)
-      
-      colnames(predict.df) = paste(all.vars(formula(i))[1], "fit", sep = ".") else 
-        
-        colnames(predict.df) = paste(all.vars(formula(i))[1], colnames(predict.df), sep = ".")
+             
+  # If se.fit = TRUE for mixed models, calculate standard errors based on fixed-effects only
+  if(se.fit == TRUE & any(class(i) %in% c("lme", "glmmPQL", "lmerMod", "glmerMod", "merModTest"))) {
     
-    # Return predictions
-    return(predict.df)
+    # Bind in predictions to new data
+    newdata = data.frame(newdata, predict.df)
+    
+    colnames(newdata)[ncol(newdata)] = all.vars(formula(i))[1]
+    
+    if(any(class(i) %in% c("lme", "glmmPQL"))) {
+      
+      Dmat = model.matrix(formula(i)[-2], newdata) 
+      
+      pvar = sqrt(diag(Dmat %*% vcov(i) %*% t(Dmat)))
+      
+    } else {
+      
+      Dmat = model.matrix(terms(i), newdata)
+      
+      pvar = diag(Dmat %*% tcrossprod(vcov(i), Dmat))
+      
+    }
+    
+    # Return list with predicted errors
+    predict.df = list(fit = predict.df, se.fit = pvar)
+    
+  }
+      
+  # If predictions are stored in a list, bind columns
+  if(class(predict.df) == "list") predict.df = do.call(data.frame, predict.df[1:2]) else
+    
+    predict.df = data.frame(predict.df)
+      # Name columns
+  if(ncol(predict.df) == 1)
+    
+    colnames(predict.df) = paste(all.vars(formula(i))[1], "fit", sep = ".") else 
+      
+      colnames(predict.df) = paste(all.vars(formula(i))[1], colnames(predict.df), sep = ".")
+  
+  # Return predictions
+  return(predict.df)
       
   } )
   
