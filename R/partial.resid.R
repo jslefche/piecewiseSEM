f418dfd91ccc8048989db19c23d040d2b84f8a2c
Jon Lefcheck <jslefche@vims.edu>
2015-07-01 11:07:46 -0400

    Fixed partial resid bug with lme4 and gls models

--- R/partial.resid.R
+++ R/partial.resid.R
@@ -22,7 +22,7 @@ partial.resid = function(.formula = y ~ x, modelList, model.control = NULL, plot
     stop("Y is a direct function of X, no partial residuals obtainable")
 
   # Get model formula 
-  rhs = formula(drop.terms(y.model$terms, grep(x, attr(y.model$terms, "term.labels")), keep.response = TRUE))
+  rhs = formula(drop.terms(terms(y.model), grep(x, attr(terms(y.model), "term.labels")), keep.response = TRUE))
 
   random.formula = get.random.formula(y.model, rhs, modelList, drop = x)
   
@@ -32,13 +32,15 @@ partial.resid = function(.formula = y ~ x, modelList, model.control = NULL, plot
   # Update model
   y.nox.model = suppressWarnings(if(is.null(random.formula)) 
     
-    update(y.model, formula = formula(rhs), control = control) else
+    update(y.model, formula(rhs), control = control) else
       
       if(any(class(y.model) %in% c("lme", "glmmPQL"))) 
         
         update(y.model, fixed = formula(rhs), random = formula(random.formula), control = control) else
           
-          update(y.model, fixed = formula(y, " ~ ", rhs, " + ", random.formula, sep = ""), control = control) )
+          update(y.model, paste(deparse(rhs), " + ", random.formula, collapse = ""), control = control) 
+    
+    )
   
   # Replace x variable as response in y model
   x.noy.model = suppressWarnings(if(is.null(random.formula)) {
@@ -48,14 +50,14 @@ partial.resid = function(.formula = y ~ x, modelList, model.control = NULL, plot
       # Try to update model
       mod = try(suppressWarnings(suppressMessages(
         update(y.model, 
-               formula = reformulate(deparse(formula(y.nox.model)[[3]]), response = x), 
+               reformulate(deparse(formula(y.nox.model)[[3]]), response = x), 
                control = control)
         ) ), silent = TRUE)
       
       if(class(mod) == "try-error")
         
         update(y.model, 
-               formula = reformulate(deparse(formula(y.nox.model)[[3]]), response = x), 
+               reformulate(deparse(formula(y.nox.model)[[3]]), response = x), 
                family = gaussian(), 
                na.action = na.omit,
                control = control) else mod 
@@ -63,7 +65,7 @@ partial.resid = function(.formula = y ~ x, modelList, model.control = NULL, plot
     } else 
       
       update(y.model, 
-             formula = reformulate(deparse(formula(y.nox.model)[[3]]), response = x), 
+             reformulate(deparse(formula(y.nox.model)[[3]]), response = x), 
              control = control)
     
   } else
@@ -72,7 +74,7 @@ partial.resid = function(.formula = y ~ x, modelList, model.control = NULL, plot
         
         update(y.model, fixed = reformulate(deparse(formula(y.nox.model)[[3]]), response = x), random = formula(random.formula), control = control) else
           
-          update(y.model, fixed = reformulate(deparse(formula(y.nox.model)[[3]]), response = x), control = control) 
+          update(y.model, reformulate(deparse(formula(y.nox.model)[[3]]), response = x), control = control) 
   
   )
 
@@ -89,11 +91,11 @@ partial.resid = function(.formula = y ~ x, modelList, model.control = NULL, plot
   if(plotit == TRUE)
     
     plot(resids.data[, 1] ~ resids.data[ ,2], 
-         xlab = ifelse(length(attr(y.nox.model$terms, "term.labels")) <= 3,
-                       ifelse(length(attr(y.nox.model$terms, "term.labels")[-1]) > 1, paste(y, paste(attr(y.nox.model$terms, "term.labels")[-1], collapse=" + "), sep = " | "), paste(y)),
+         xlab = ifelse(length(attr(terms(y.nox.model), "term.labels")) <= 3,
+                       ifelse(length(attr(terms(y.nox.model), "term.labels")[-1]) > 1, paste(y, paste(attr(terms(y.nox.model), "term.labels")[-1], collapse=" + "), sep = " | "), paste(y)),
                        paste(y, "| others") ),
-         ylab = ifelse(length(attr(x.noy.model$terms, "term.labels")) <= 3,
-                       ifelse(length(attr(x.noy.model$terms, "term.labels")[-1]) > 1, paste(x, paste(attr(x.noy.model$terms, "term.labels")[-1], collapse=" + "), sep = " | "), paste(x)),
+         ylab = ifelse(length(attr(terms(x.noy.model), "term.labels")) <= 3,
+                       ifelse(length(attr(terms(x.noy.model), "term.labels")[-1]) > 1, paste(x, paste(attr(terms(x.noy.model), "term.labels")[-1], collapse=" + "), sep = " | "), paste(x)),
                        paste(x, "| others") )) 
     
   if(plotit == TRUE & plotreg == TRUE) abline(lm(resids.data[ ,1] ~ resids.data[ ,2]), col = "red", lwd = 2)
