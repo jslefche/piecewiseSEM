48e63dfe46aa4982c8e3977e6a00f665e1e17dc3
Jon Lefcheck <jslefche@vims.edu>
2015-08-24 15:45:20 -0400

    Fixed issue with odd independence claims

--- R/get.basis.set.R
+++ R/get.basis.set.R
@@ -1,44 +1,28 @@
 get.basis.set = function(amat) {
   
-  ret = lapply(1:ncol(amat), function(i) {
+  # Sort adjacency matrix by parent to child nodes
+  amat = topSort(amat)
+  
+  ret = lapply(1:ncol(amat), function(j) {
     
-    lapply(1:nrow(amat), function(j) {
+    lapply(j:nrow(amat), function(i) {
       
-      # If any relationship is directional or the variable predicts itself, return NULL
-      if(amat[j, i] != 0 | amat[i, j] != 0 | i == j | sum(amat[j, ]) == 0) NULL else {
+      if(amat[j, i] != 0 | j == i) NULL else {  
         
-        # Get directed relationship
+        # Get variables for independence test
         dsep = unlist(dimnames(amat[j, i, drop = FALSE]))
-    
-        # Determine if direct relationship is cylical
-        resp = names(amat[, dsep[1]][amat[, dsep[1]] > 0])
         
-        A = FALSE
+        # Get vector of conditional variables
+        cond.var = c(
+          rownames(amat)[which(amat[, dsep[1], drop = FALSE] == 1)],
+          rownames(amat)[which(amat[, dsep[2], drop = FALSE] == 1)]
+        )
         
-        while(A == FALSE) {
-
-          nm = names(amat[, resp[length(resp)]][amat[, resp[length(resp)]] > 0])
-          
-          if(length(nm) == 0) A = TRUE else resp = c(resp, nm)
-          
-        }
-          
-        # If relationship is cyclical then return NULL
-        if(any(resp %in% dsep[-1])) NULL else {
-          
-          # Get vector of conditional variables
-          cond.var = c(
-            rownames(amat)[which(amat[, dsep[1], drop = FALSE] == 1)],
-            rownames(amat)[which(amat[, dsep[2], drop = FALSE] == 1)]
-          )
-       
-          # Return full independence claim
-          c(
-            dsep,
-            cond.var[!cond.var %in% rownames(amat[j, , drop = FALSE])]
-          )
-            
-        }
+        # Remove conditional variables already in the independence claim
+        cond.var = cond.var[!cond.var %in% dsep]
+        
+        # Return full independence claim
+        c(dsep, cond.var)
         
       }
       
