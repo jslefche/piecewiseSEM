9614542df16fdbb5f73f334692e2849c3ce22442
Jon Lefcheck <jslefche@vims.edu>
2016-09-19 14:00:46 -0400

    Fixed sorting issue for DAG

--- R/get.sort.dag.R
+++ R/get.sort.dag.R
@@ -4,39 +4,25 @@ get.sort.dag = function(formulaList) {
   amat = get.dag(formulaList)
   
   # Get predictors where colSums == 0
-  col.zero = colnames(amat)[colSums(amat) == 0]
+  idx = unname(which(colSums(amat) == 0))
 
-  # Put those predictors at the beginning of the index
-  idx = col.zero
-  
   # Of remaining variables, look for only those with links to the predictors in col.zero
-  first.pred = colnames(amat[, !colnames(amat) %in% idx, drop = FALSE])[
-    colSums(amat[!rownames(amat) %in% col.zero, !colnames(amat) %in% idx, drop = FALSE]) == 0 & 
-      colSums(amat[rownames(amat) %in% col.zero, !colnames(amat) %in% idx, drop = FALSE]) != 0 ]
+  idx. = 
+    which(!colnames(amat) %in% colnames(amat)[idx])[
+      colSums(amat[!colnames(amat) %in% colnames(amat)[idx], !colnames(amat) %in% colnames(amat)[idx], drop = FALSE]) == 0 & 
+        colSums(amat[colnames(amat) %in% colnames(amat)[idx], !colnames(amat) %in% colnames(amat)[idx], drop = FALSE]) != 0]
   
-  first.pred = names(sort(colSums(amat[, first.pred, drop = FALSE]), decreasing = TRUE))
+  # Sort by increasing position in amat
+  pos = apply(amat[, idx., drop = FALSE], 2, function(x) max(which(x == 1)))
+    
+  idx. = c(idx, which(colnames(amat) %in% names(pos[order(pos)])))
+ 
+  # Sort remaining variables by increasing position in amat
+  pos. = apply(amat[, !colnames(amat) %in% colnames(amat)[idx.], drop = FALSE], 2, function(x) max(which(x == 1)))
   
-  idx = c(idx, first.pred)
-  
-  # Of remaining variables, look for those with links in both col.zero and !col.zero
-  second.pred = colnames(amat[, !colnames(amat) %in% idx, drop = FALSE])[
-    colSums(amat[!rownames(amat) %in% col.zero, !colnames(amat) %in% idx, drop = FALSE]) != 0 & 
-      colSums(amat[rownames(amat) %in% col.zero, !colnames(amat) %in% idx, drop = FALSE]) != 0 ]
+  idx.. = c(idx., which(colnames(amat) %in% names(pos.[order(pos.)])))
 
-  second.pred = names(sort(colSums(amat[, second.pred, drop = FALSE]), decreasing = TRUE))
-  
-  idx = c(idx, second.pred)
-
-  # Finally, only variables with links in !col.zero
-  final.pred = colnames(amat[, !colnames(amat) %in% idx, drop = FALSE])[
-    colSums(amat[!rownames(amat) %in% col.zero, !colnames(amat) %in% idx, drop = FALSE]) != 0 & 
-      colSums(amat[rownames(amat) %in% col.zero, !colnames(amat) %in% idx, drop = FALSE]) == 0 ]
-
-  final.pred = names(sort(colSums(amat[, final.pred, drop = FALSE]), decreasing = TRUE))
-  
-  idx = c(idx, final.pred)
-             
   # Return adjacency matrix, sorted
-  amat[idx, idx]
+  amat[idx.., idx..]
   
 }
