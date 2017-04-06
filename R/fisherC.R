4af9de06d67684c9846bb6889b5ddfc154f395d5
Jon Lefcheck <jslefche@vims.edu>
2017-04-06 17:00:44 -0400

    Added argument for additional independence claims

--- R/fisherC.R
+++ R/fisherC.R
@@ -2,29 +2,39 @@
 #'
 #' @param dTable a list of structural equations
 
-fisherC <- function(dTable) {
+fisherC <- function(dTable, claims = NULL) {
 
   if(length(dTable) == 0) {
 
     C <- 0
 
-    p <- 1
-
     DF <- 0
+
+    P <- 1
 
   } else {
 
-    if(any(dTable$P.value == 0)) dTable$P.value = dTable$P.Value + 1e-20
+    ps <- dTable$P.Value
 
-    C <- -2*sum(log(dTable$P.Value))
+    if(!is.null(claims)) {
 
-    p <- 1 - pchisq(C, 2*nrow(dTable))
+      ps <- ps + claims
 
-    DF <- 2*nrow(dTable)
+      message("Fisher's C has been adjusted to include additional claims not shown in the tests of directed separation.")
+
+      }
+
+    if(any(ps == 0)) ps <- ps + 1e-20
+
+    C <- -2 * sum(log(ps))
+
+    DF <- 2 * length(ps)
+
+    P <- 1 - pchisq(C, 2 * DF)
 
   }
 
-  ret <- data.frame(Fisher.C = C, df = DF, P.Value = p)
+  ret <- data.frame(Fisher.C = C, df = DF, P.Value = P)
 
   ret[, which(sapply(ret, is.numeric))] <- round(ret[, which(sapply(ret, is.numeric))], 3)
 
