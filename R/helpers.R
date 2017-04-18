fcb13a62640ae1c3a2d5aeb4950600ec21627928
Jon Lefcheck <jslefche@vims.edu>
2017-04-18 17:07:54 -0400

    Reordered helpers alphabetically

--- R/helpers.R
+++ R/helpers.R
@@ -1,25 +1,3 @@
-#' Get list of formula from a `sem` object
-#' If remove = TRUE, take out non-evaluated formula
-listFormula <- function(modelList, remove = FALSE) {
-
-  modelList <- modelList[!sapply(modelList, function(x) any(class(x) %in% c("matrix", "data.frame")))]
-
-  if(!all(class(modelList) %in% c("psem", "list"))) modelList <- list(modelList)
-
-  fList <- lapply(modelList, function(i) if(any(class(i) %in% c("formula.cerror"))) i else formula(i) )
-
-  if(remove == TRUE) {
-
-    l <- sapply(modelList, function(i) any(class(i) %in% c("formula", "formula.cerror")))
-
-    fList <- fList[!l]
-
-  }
-
-  return(fList)
-
-}
-
 #' Remove random effects from all.vars
 all.vars.merMod <- function(.formula) {
 
@@ -29,51 +7,17 @@ all.vars.merMod <- function(.formula) {
 
     gsub(" " , "", unlist(strsplit(.formula, "~~"))) else {
 
-    n <- rownames(attr(terms(.formula), "factors"))
+      n <- rownames(attr(terms(.formula), "factors"))
 
-    if(any(grepl("\\|", n))) {
+      if(any(grepl("\\|", n))) {
 
-      f <- lme4::nobars(.formula)
+        f <- lme4::nobars(.formula)
 
-      all.vars(f)
+        all.vars(f)
 
-    } else all.vars(.formula)
+      } else all.vars(.formula)
 
     }
-
-}
-
-
-#' Do not print attributes with custom functions
-print.attr <- function(x) {
-
-  attributes(x) <- NULL
-
-  noquote(x)
-
-}
-
-#' Assess significance
-isSig <- function(p) {
-
-  ifelse(p > 0.01 & p < 0.05, "*",
-       ifelse(p > 0.001 & p <= 0.01, "**",
-              ifelse(p <= 0.001, "***", "")))
-
-}
-
-#' Get random effects from merMod
-onlyBars <- function(.formula) {
-
-  paste(
-
-    sapply(lme4::findbars(.formula), function(x)
-
-      paste0("(", deparse(x), ")")
-
-    ),
-
-    collapse = " + ")
 
 }
 
@@ -85,64 +29,6 @@ findbars.lme <- function(model) {
   rand <- gsub(".*\\|(.*)", "\\1", as.character(rand)[2])
 
   strsplit(gsub(" " , "", rand), "\\/")[[1]]
-
-}
-
-#' Get random effects variance-covariance from lme
-getVarCov. <- function(model) {
-
-  vc <- try(getVarCov(model), silent = TRUE)
-
-  if(any(class(vc) == "try-error")) {
-
-    vc <- VarCorr(model)
-
-    v <- suppressWarnings(as.numeric(vc[, 1]))
-
-    names(v) <- gsub(" =", "", rownames(vc))
-
-    vm <- as.list(na.omit(v[-length(v)]))
-
-    vl <- lapply(1:length(vm), function(i) matrix(vm[[i]], dimnames = list(names(vm)[i], names(vm)[i])))
-
-    names(vl) <- names(which(is.na(v)))
-
-    vl
-
-  } else list(vc)
-
-}
-
-#' Recompute p-values using Kenward-Rogers approximation
-KRp <- function(model, vars, intercepts = FALSE) {
-
-  ret <- sapply(vars, function(x) {
-
-    reducMod <- update(model, as.formula(paste(". ~ . -", x)))
-
-    kr <- suppressWarnings(pbkrtest::KRmodcomp(model, reducMod))
-
-    d <- round(kr$stats$ddf, 2)
-
-    p <- kr$stats$p.valueU
-
-    c(d, p)
-
-  } )
-
-  if(intercepts == TRUE) {
-
-    reducMod <- update(model, as.formula(paste("~ 0 + .")))
-
-    kr <- suppressWarnings(pbkrtest::KRmodcomp(model, reducMod))
-
-    d <- kr$stats$ddf
-
-    p <- kr$stats$p.valueU
-
-    cbind(`(Intercept)` = c(d, p), ret)
-
-  } else ret
 
 }
 
@@ -188,5 +74,120 @@ getData. <- function(modelList) {
   data <- data[, !duplicated(colnames(data), fromLast = TRUE)]
 
   return(data)
+
+}
+
+
+#' Get random effects variance-covariance from lme
+getVarCov. <- function(model) {
+
+  vc <- try(getVarCov(model), silent = TRUE)
+
+  if(any(class(vc) == "try-error")) {
+
+    vc <- VarCorr(model)
+
+    v <- suppressWarnings(as.numeric(vc[, 1]))
+
+    names(v) <- gsub(" =", "", rownames(vc))
+
+    vm <- as.list(na.omit(v[-length(v)]))
+
+    vl <- lapply(1:length(vm), function(i) matrix(vm[[i]], dimnames = list(names(vm)[i], names(vm)[i])))
+
+    names(vl) <- names(which(is.na(v)))
+
+    vl
+
+  } else list(vc)
+
+}
+
+#' Assess significance
+isSig <- function(p) {
+
+  ifelse(p > 0.01 & p < 0.05, "*",
+         ifelse(p > 0.001 & p <= 0.01, "**",
+                ifelse(p <= 0.001, "***", "")))
+
+}
+
+#' Recompute p-values using Kenward-Rogers approximation
+KRp <- function(model, vars, intercepts = FALSE) {
+
+  ret <- sapply(vars, function(x) {
+
+    reducMod <- update(model, as.formula(paste(". ~ . -", x)))
+
+    kr <- suppressWarnings(pbkrtest::KRmodcomp(model, reducMod))
+
+    d <- round(kr$stats$ddf, 2)
+
+    p <- kr$stats$p.valueU
+
+    c(d, p)
+
+  } )
+
+  if(intercepts == TRUE) {
+
+    reducMod <- update(model, as.formula(paste("~ 0 + .")))
+
+    kr <- suppressWarnings(pbkrtest::KRmodcomp(model, reducMod))
+
+    d <- kr$stats$ddf
+
+    p <- kr$stats$p.valueU
+
+    cbind(`(Intercept)` = c(d, p), ret)
+
+  } else ret
+
+}
+
+#' Get list of formula from a `sem` object
+#' If remove = TRUE, take out non-evaluated formula
+listFormula <- function(modelList, remove = FALSE) {
+
+  modelList <- modelList[!sapply(modelList, function(x) any(class(x) %in% c("matrix", "data.frame")))]
+
+  if(!all(class(modelList) %in% c("psem", "list"))) modelList <- list(modelList)
+
+  fList <- lapply(modelList, function(i) if(any(class(i) %in% c("formula.cerror"))) i else formula(i) )
+
+  if(remove == TRUE) {
+
+    l <- sapply(modelList, function(i) any(class(i) %in% c("formula", "formula.cerror")))
+
+    fList <- fList[!l]
+
+  }
+
+  return(fList)
+
+}
+
+
+#' Get random effects from merMod
+onlyBars <- function(.formula) {
+
+  paste(
+
+    sapply(lme4::findbars(.formula), function(x)
+
+      paste0("(", deparse(x), ")")
+
+    ),
+
+    collapse = " + ")
+
+}
+
+#' Do not print attributes with custom functions
+print.attr <- function(x) {
+
+  attributes(x) <- NULL
+
+  noquote(x)
 
 }
