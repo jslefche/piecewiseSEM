49bb8eead832721627b9f5bd0ed63c53bb9c5c79
Jon Lefcheck <jlefcheck@bigelow.org>
2018-06-06 12:53:05 -0400

    Added data documentation

---
+++ R/data.R
@@ -0,0 +1,38 @@
+#' Data set from Shipley (2009)
+#'
+#' \itemize{
+#' \item{site}{Site of observation}
+#' \item{tree}{Tree of observation}
+#' \item{lat}{Latitude}
+#' \item{year}{Year of observation}
+#' \item{Date}{Julian date of first bud burst}
+#' \item{DD}{Cumulative degree days until first bud burst}
+#' \item{Growth}{Increase in stem diameter}
+#' \item{Survival}{Plant species richness}
+#' \item{Live}{Alive (1) or dead (0)}
+#' }
+#' @format A \code{data.frame} with 1900 observations of 9 variables.
+#' @name shipley
+#' @docType data
+#' @keywords data
+#' @examples head(shipley)
+"shipley"
+
+#' Data set from Keeley et al.
+#'
+#' \itemize{
+#' \item{distance}{Distance to coast}
+#' \item{elev}{Elevation from sea level}
+#' \item{abiotic}{Abiotic favorability}
+#' \item{age}{Age of stand before fire}
+#' \item{hetero}{Plot heterogeneity}
+#' \item{firesev}{Severity of fire}
+#' \item{cover}{Cover of plants}
+#' \item{rich}{Plant species richness}
+#' }
+#' @format A \code{data.frame} with 90 observations of 8 variables.
+#' @name keeley
+#' @docType data
+#' @keywords data
+#' @examples head(keeley)
+"keeley"
