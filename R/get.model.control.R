a60bc10af29b95599155fddd598fd842efa01729
Jon Lefcheck <jlefcheck@bigelow.org>
2018-06-11 10:00:15 -0400

    Revert "Merge branch '2.0'"
    This reverts commit a406e7bc18c8b49fc903c142fd8be234a388a786, reversing
    changes made to 74fd32c2917fc5770affd0a7275c13bc962df849.

---
+++ R/get.model.control.R
@@ -0,0 +1,45 @@
+# Fixes issues with nlme updated; thanks to Martin Maechler, 2016-01-19
+
+get.model.control = function(model, model.control) {
+  
+  model.class = if(inherits(model, "merModLmerTest")) "lmerMod" else class(model)
+  
+  # Match model control list with appropriate model class for basis model
+  if(is.null(model.control)) {
+    
+    if(inherits(model, "glm")) glm.control()
+    
+    else if(inherits(model, "gls")) glsControl()
+    
+    else if(any(class(model) %in% c("lme", "glmmPQL", "glmmadmb"))) lmeControl()
+    
+    else if(any(class(model) %in% c("lmerMod", "merModLmerTest"))) lmerControl()
+    
+    else if(inherits(model, "glmerMod")) glmerControl()
+    
+  } else {
+    
+    ## assume model.control = a *list* of control lists
+    control.classes = lapply(model.control, function(i)
+      
+      gsub("(.*)Control", "\\1", class(i))[1] )
+    
+    if(any(control.classes %in% (M.cl <- gsub("(.*)Mod", "\\1", model.class))))
+      
+      model.control[[control.classes %in% M.cl]] else
+        
+        if(inherits(model, "glm"))
+          
+          model.control[[sapply(model.control, length) >= 3]] else
+            
+            if(inherits(model, "gls")) 
+              
+              model.control[[sapply(model.control, length) >= 13]] else
+                
+                if(any(class(model) %in% c("lme", "glmmPQL", "glmmadmb"))) 
+                  
+                  model.control[[sapply(model.control, length) >= 15]]
+    
+  }
+  
+}
--- cran-comments.md
+++ cran-comments.md
@@ -1,12 +1,12 @@
 ## Update package
-This is a major update introducing new and enhanced functionality.
+This is a bug fix update.
 
 ## R CMD check results
 There were no ERRORs or WARNINGs. 
 
 ## NOTES 
-1 NOTE corresponding to cran-comments.md in the top level.
+* There were no notes.
 
 ## Test environments
-* local OS X install, R 3.5.0
+* local OS X install, R 3.3.2
 * win-builder (R-devel and R-release)
