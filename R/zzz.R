1e026cbf7c20822d792cb3e4c3958c1df8108d1b
Jon Lefcheck <lefcheckj@si.edu>
2018-09-11 09:40:36 -0400

    Updated contact info

--- R/zzz.R
+++ R/zzz.R
@@ -4,6 +4,6 @@
   This is piecewiseSEM version 2.0.3\n
 
   If you have used the package before, it is strongly recommended you read Section 3 of the vignette('piecewiseSEM') to familiarize yourself with the new syntax\n
-  Questions or bugs can be addressed to <jlefcheck@bigelow.org>")
+  Questions or bugs can be addressed to <LefcheckJ@si.edu>")
 
   }
