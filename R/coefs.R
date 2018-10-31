22e66757be71c58af086ece934a30df9d72f65b4
Jon Lefcheck <jlefcheck@bigelow.org>
2018-10-31 10:43:48 -0400

    Final cleanup for emmeans

--- R/coefs.R
+++ R/coefs.R
@@ -39,10 +39,10 @@
 #' reports model-estimated means in the \code{Estimate} column. This is done so all
 #' n paths in the corresponding path diagram have assignable values.
 #' 
-#' The means are generated using function \code{lsmeans} in the \code{emmeans} package. 
+#' The means are generated using function \code{emmeans} in the \code{emmeans} package. 
 #' Pairwise contrasts  are further conducted among all levels using the default  
 #' correction for multiple testing. The results of those comparisons are given in the 
-#' significance codes (e.g., "a", "b", "ab") as reported in the \code{lsmeans::cld} function.
+#' significance codes (e.g., "a", "b", "ab") as reported in the \code{emmeans::cld} function.
 #'
 #' @param modelList A list of structural equations.
 #' @param standardize The type of standardization: \code{none}, \code{scale}, \code{range}.
@@ -59,7 +59,7 @@
 #' @references Grace, J.B., Johnson, D.A., Lefcheck, J.S., and Byrnes, J.E.
 #' "Standardized Coefficients in Regression and Structural Models with Binary Outcomes." 
 #' Ecosphere 9(6): e02283.
-#' @seealso \code{\link{KRmodcomp}}, \code{\link{lsmeans}}, \code{\link{CLD}}
+#' @seealso \code{\link{KRmodcomp}}, \code{\link{emmeans}}, \code{\link{CLD}}
 #' 
 #' @export
 #'
@@ -132,7 +132,7 @@ unstdCoefs <- function(modelList, data = NULL, test.type = "II", intercepts = FA
 #' @keyword internal
 #' 
 #' @export
-#' 
+#'
 getCoefficients <- function(model, data, test.type = "II") {
   
   vars <- all.vars.merMod(formula(model))
@@ -185,7 +185,7 @@ getCoefficients <- function(model, data, test.type = "II") {
     
     anovaTable <- as.data.frame(car::Anova(model, type = test.type))
   
-    levs <- lapply(factorVars, function(j) emmeans::lsmeans(model, list(formula(paste("pairwise ~", j)))))
+    levs <- lapply(factorVars, function(j) emmeans::emmeans(model, list(formula(paste("pairwise ~", j)))))
   
     ret <- do.call(rbind, lapply(levs, function(j) {
       
@@ -195,7 +195,7 @@ getCoefficients <- function(model, data, test.type = "II") {
        
        atab <- anovaTable[which(rownames(anovaTable) == names(out)[1]), ]
        
-       atab. <- data.frame(lsmean = NA, SE = NA, DF = atab$Df, crit.value = atab[, 1], P.value = atab[, ncol(atab)], sig = isSig(atab[, ncol(atab)]))
+       atab. <- data.frame(emmean = NA, SE = NA, DF = atab$Df, crit.value = atab[, 1], P.value = atab[, ncol(atab)], sig = isSig(atab[, ncol(atab)]))
        
        rownames(atab.) <- rownames(atab) 
        
