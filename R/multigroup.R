1de74dc938b30cfee1a2311a2d34bbf87dfb7078
Jon Lefcheck <lefcheckj@si.edu>
2018-11-02 11:08:43 -0400

    Updated to multigroup

--- R/multigroup.R
+++ R/multigroup.R
@@ -1,11 +1,27 @@
 #' Multigroup Analysis for Piecewise SEM
 #' 
-#' @param modelList
-#' @param group
+#' @param object a list of structural equations
+#' @param group the name of the grouping variable
 #' 
-#' @return 
+#' @author Jon Lefcheck <LefcheckJ@@si.edu>
+#' 
+#' @examples
+#' data(meadows)
+#' 
+#' jutila <- psem(
+#' lm(rich ~ elev + mass, data = meadows),
+#' lm(mass ~ elev, data = meadows)
+#' )
+#' 
+#' jutila.multigroup <- multigroup(jutila, group = "grazed")
+#' 
+#' jutila.multigroup
+#' 
+#' @export
 #' 
 multigroup <- function(modelList, group) {
+  
+  name <- deparse(match.call()$modelList)
   
   data <- modelList$data
   
@@ -45,23 +61,30 @@ multigroup <- function(modelList, group) {
   
   global$Predictor <- gsub(paste0("(.*):|", group, "(.*)"), "\\1", global$Predictor)
   
-  # alter output for each group if interaction is non-significant (Assign global value)
-  newCoefsList <- lapply(coefsList, function(i) {
-    
-    i[which(i$Response %in% global$Response & i$Predictor %in% global$Predictor), ] <-
-      coefTable[which(coefTable$Response %in% global$Response & coefTable$Predictor %in% global$Predictor), ]
+  if(nrow(global) == nrow(anovaInts)) newCoefsList <- list(global = coefTable) else {
   
-    i[, ncol(i)] <- ifelse(i$Response %in% global$Response & i$Predictor %in% global$Predictor, "c", "")
+    # alter output for each group if interaction is non-significant (Assign global value)
+    newCoefsList <- lapply(coefsList, function(i) {
+      
+      suppressWarnings(
+        i[which(i$Response %in% global$Response & i$Predictor %in% global$Predictor), ] <-
+        coefTable[which(coefTable$Response %in% global$Response & coefTable$Predictor %in% global$Predictor), ]
+      )
     
-    i[, ncol(i) + 1] <- isSig(i$P.Value)
+      i[, ncol(i)] <- ifelse(i$Response %in% global$Response & i$Predictor %in% global$Predictor, "c", "")
+      
+      i[, ncol(i) + 1] <- isSig(i$P.Value)
+      
+      names(i)[(ncol(i) - 1):ncol(i)] <- ""
+      
+      return(i) 
+      
+    } )
     
-    names(i)[(ncol(i) - 1):ncol(i)] <- ""
-    
-    return(i) 
-    
-  } )
+  }
 
   ret <- list(
+    name = name,
     group = group,
     global = global,
     anovaInts = anovaInts,
@@ -77,12 +100,10 @@ multigroup <- function(modelList, group) {
 #' Print multigroup
 #' 
 print.multigroup.psem <- function(x, ...) {
+
+  cat("\nStructural Equation Model of", x$name, "\n")
   
-  name <- deparse(substitute(object))
-  
-  cat("\nStructural Equation Model of", name, "\n")
-  
-  cat("\nGroups =", x$group, ":", paste(names(x$group.coefs), collapse = " "))
+  cat("\nGroups =", x$group, ":", paste(names(x$group.coefs), collapse = ", "))
   
   cat("\n")
   
@@ -90,23 +111,35 @@ print.multigroup.psem <- function(x, ...) {
   
   cat("\n", captureTable(x$anovaInts))
   
-  for(i in 1:nrow(x$global)) cat("\n", paste(x$global[i, "Predictor"], "->", x$global[i, "Response"], "constrained to the global model"), "\n")
+  if(nrow(x$global) == 0) cat("\n No paths constrained to the global model") else {
+    
+    for(i in 1:nrow(x$global)) cat("\n", paste(x$global[i, "Predictor"], "->", x$global[i, "Response"], "constrained to the global model"), "\n")
   
+  }
+    
   cat("\n---\n\n")
   
-  for(i in names(x$group.coefs)) {
+  if(length(x$group.coefs) == 1) {
     
-    cat(paste("Group:", i), "coefficients \n")
+    cat("Global coefficients:\n")
     
-    cat("\n", captureTable(x$group.coefs[[i]]), "\n")
+    cat("\n", captureTable(x$group.coefs[[1]]), "\n")
     
+    } else { 
+      
+      for(i in names(x$group.coefs)) { 
+        
+        cat(paste("Group", i), "coefficients: \n")
+    
+        cat("\n", captureTable(x$group.coefs[[i]]), "\n")
+        
+      }
   }
   
   cat("Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05    c = constrained")
   
-  cat("\n---\n\n")
+  cat("\n\n---\n\n")
 
   invisible(x)
   
 }
-
