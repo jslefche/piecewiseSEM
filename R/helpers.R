fde0301aae3d4a3833545308a65bcaeb4cf1ef8e
Jon Lefcheck <LefcheckJ@si.edu>
2019-03-14 13:23:51 -0400

    Merged RemoveData and helpers

--- R/helpers.R
+++ R/helpers.R
@@ -529,20 +529,20 @@ print.attr <- function(x, ...) {
 #' formulas = 2, remove only formula but keep correlated errors
 #' formulas = 3, remove correlated errors but keep formula
 #' 
-#' @keywords internal
+#' @keywords export
 #' 
 removeData <- function(modelList, formulas = 0) {
-
+  
   remove <- c("character", "matrix", "data.frame", "SpatialPointsDataFrame", "comparative.data")
-
+  
   if(formulas == 1) remove <- c(remove, "formula", "formula.cerror")
-
+  
   if(formulas == 2) remove <- c(remove, "formula")
-
+  
   if(formulas == 3) remove <- c(remove, "formula.cerror")
-
+  
   modelList[!sapply(modelList, function(x) any(class(x) %in% remove))]
-
+  
 }
 
 #' Strip transformations
