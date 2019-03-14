aa46d1f5235dad89f0b785d28d32cc3c8209f4eb
Jon Lefcheck <LefcheckJ@si.edu>
2019-03-14 10:23:50 -0400

    Bumped last updated date

--- R/piecewiseSEM-package.R
+++ R/piecewiseSEM-package.R
@@ -10,7 +10,7 @@
 #' merModLmerTest, glmerMod}.
 #'
 #' \tabular{ll}{ Package: \tab piecewiseSEM\cr Type: \tab Package\cr Version:
-#' \tab 2.1.0\cr Date: \tab 2019-01-30\cr Depends: \tab R (>= 3.5.0), nlme,
+#' \tab 2.1.0\cr Date: \tab 2019-03-14\cr Depends: \tab R (>= 3.5.0), nlme,
 #' pbkrtest\cr Suggests: \tab MASS, lme4\cr License: \tab MIT\cr } The primary
 #' functions in the package are \code{\link{psem}} which unites structural
 #' equations in a single model. \code{summary.psem} can be used on an object of
