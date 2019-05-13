8d2aaa965af50bfbd0f9fc2d17f7b0fce77fa813
Jon Lefcheck <LefcheckJ@si.edu>
2019-05-13 14:05:35 -0500

    Fixed issue with formatpsem returning a matrix

--- R/psem.R
+++ R/psem.R
@@ -75,11 +75,11 @@ formatpsem <- function(x) {
 
   }
   
-  vars <- as.vector(sapply(removeData(x, formulas = 1), function(x) unname(all.vars_notrans(x))))
+  vars <- as.vector(unlist(sapply(removeData(x, formulas = 1), function(x) unname(all.vars_notrans(x)))))
 
   vars <- vars[!duplicated(vars) & !grepl("\\:", vars)]
   
-  t_vars <- as.vector(sapply(removeData(x, formulas = 1), function(x) unname(all.vars_trans(x))))
+  t_vars <- as.vector(unlist(sapply(removeData(x, formulas = 1), function(x) unname(all.vars_trans(x)))))
   
   t_vars <- t_vars[!duplicated(t_vars) & !grepl("\\:", t_vars)]
   
