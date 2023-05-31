917f5bd098ea3c1fee13658f92e34882160b4849
Jon Lefcheck <lefcheckj@si.edu>
2023-05-31 16:11:13 -0400

    Minor cleanup for push to CRAN

--- R/zzz.R
+++ R/zzz.R
@@ -3,6 +3,6 @@
   packageStartupMessage("
   This is piecewiseSEM version 2.3.0.\n
 
-  Questions or bugs can be addressed to <LefcheckJ@si.edu>.")
+  Questions or bugs can be addressed to <jlefcheck@umces.edu>.")
   
   }
